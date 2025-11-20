c13dc43ef3fa32694f9d197c4ac319bff163c259
loopbutinfite <bestrada8800@gmail.com>
2025-11-19 17:00:35 -0800

    first commit and initial publish

---
+++ README.md
@@ -0,0 +1,9 @@
+## Brandon Estrada
+
+## Date Revised: November 19, 2025
+
+## Time Tracker
+
+## Description: Created a an HTML responsive website from a prototype that utilizes BootStrap's library of components and custom css classes. The project also has custom css classes that overwrite BootStrap's css presets and psuedo CSS classes for styling and to get it close to the prototype. As well as, has media queries that allow the website to be responsive for both desktop and mobile devices. The project also has a JavaScript script file that calls upon data from a JSON file and is linked to the HTML elements.
+
+## Peer Review (Name) -
---
+++ scripts/app.js
@@ -0,0 +1,76 @@
+import data from "../data/data.json" with {type:"json"};
+
+//Create our variables that will make the Daily, Weekly, and Monthly clickable
+const dailyStats = document.getElementById("dailyStats");
+const weeklyStats = document.getElementById("weeklyStats");
+const monthlyStats = document.getElementById("monthlyStats");
+//End of variable for daily, weekly, monthly stats
+
+//Create our variables for the current and previous times inside the activity cards.
+const workCurrentTime = document.getElementById("workCurrentTime");
+const workPreviousTime = document.getElementById("workPreviousTime");
+const playCurrentTime = document.getElementById("playCurrentTime");
+const playPreviousTime = document.getElementById("playPreviousTime");
+const studyCurrentTime = document.getElementById("studyCurrentTime");
+const studyPreviousTime = document.getElementById("studyPreviousTime");
+const exerciseCurrentTime = document.getElementById("exerciseCurrentTime");
+const exercisePreviousTime = document.getElementById("exercisePreviousTime");
+const socialCurrentTime = document.getElementById("socialCurrentTime");
+const socialPreviousTime = document.getElementById("socialPreviousTime");
+const selfcareCurrentTime = document.getElementById("selfcareCurrentTime");
+const selfcarePreviousTime = document.getElementById("selfcarePreviousTime");
+//End of our variables for current and previous times
+
+console.log(data[0].timeframes.daily.current) //Displays the current time for Work from the JSON file
+console.log(data[0].timeframes.daily.previous) //Displays the previous time for Work from the JSON file
+//event listener that invokes a function when user clicks on Daily.
+dailyStats.addEventListener("click", function () {
+    workCurrentTime.innerText = `${data[0].timeframes.daily.current}hrs`; //Work
+    workPreviousTime.innerText = `Last Week - ${data[0].timeframes.daily.previous}hrs`
+    playCurrentTime.innerText = `${data[1].timeframes.daily.current}hrs`; //Play
+    playPreviousTime.innerText = `Last Week - ${data[1].timeframes.daily.previous}hrs`
+    studyCurrentTime.innerText = `${data[2].timeframes.daily.current}hrs`; //Study
+    studyPreviousTime.innerText = `Last Week - ${data[2].timeframes.daily.previous}hrs`
+    exerciseCurrentTime.innerText = `${data[3].timeframes.daily.current}hrs`; //Exercise
+    exercisePreviousTime.innerText = `Last Week - ${data[3].timeframes.daily.previous}hrs`
+    socialCurrentTime.innerText = `${data[4].timeframes.daily.current}hrs`; //Social
+    socialPreviousTime.innerText = `Last Week - ${data[4].timeframes.daily.previous}hrs`
+    selfcareCurrentTime.innerText = `${data[5].timeframes.daily.current}hrs`; //Self Care
+    selfcarePreviousTime.innerText = `Last Week - ${data[5].timeframes.daily.previous}hrs`
+});
+
+console.log(data[0].timeframes.weekly.current)
+console.log(data[0].timeframes.weekly.previous)
+
+weeklyStats.addEventListener("click", function () {
+    workCurrentTime.innerText = `${data[0].timeframes.weekly.current}hrs`;
+    workPreviousTime.innerText = `Last Week - ${data[0].timeframes.weekly.previous}hrs`
+    playCurrentTime.innerText = `${data[1].timeframes.weekly.current}hrs`;
+    playPreviousTime.innerText = `Last Week - ${data[1].timeframes.weekly.previous}hrs`
+    studyCurrentTime.innerText = `${data[2].timeframes.weekly.current}hrs`;
+    studyPreviousTime.innerText = `Last Week - ${data[2].timeframes.weekly.previous}hrs`
+    exerciseCurrentTime.innerText = `${data[3].timeframes.weekly.current}hrs`;
+    exercisePreviousTime.innerText = `Last Week - ${data[3].timeframes.weekly.previous}hrs`
+    socialCurrentTime.innerText = `${data[4].timeframes.weekly.current}hrs`;
+    socialPreviousTime.innerText = `Last Week - ${data[4].timeframes.weekly.previous}hrs`
+    selfcareCurrentTime.innerText = `${data[5].timeframes.weekly.current}hrs`;
+    selfcarePreviousTime.innerText = `Last Week - ${data[5].timeframes.weekly.previous}hrs`
+});
+
+console.log(data[0].timeframes.monthly.current)
+console.log(data[0].timeframes.monthly.previous)
+
+monthlyStats.addEventListener("click", function (){
+    workCurrentTime.innerText = `${data[0].timeframes.monthly.current}hrs`;
+    workPreviousTime.innerText = `Last Week - ${data[0].timeframes.monthly.previous}hrs`
+    playCurrentTime.innerText = `${data[1].timeframes.monthly.current}hrs`;
+    playPreviousTime.innerText = `Last Week - ${data[1].timeframes.monthly.previous}hrs`
+    studyCurrentTime.innerText = `${data[2].timeframes.monthly.current}hrs`;
+    studyPreviousTime.innerText = `Last Week - ${data[2].timeframes.monthly.previous}hrs`
+    exerciseCurrentTime.innerText = `${data[3].timeframes.monthly.current}hrs`;
+    exercisePreviousTime.innerText = `Last Week - ${data[3].timeframes.monthly.previous}hrs`
+    socialCurrentTime.innerText = `${data[4].timeframes.monthly.current}hrs`;
+    socialPreviousTime.innerText = `Last Week - ${data[4].timeframes.monthly.previous}hrs`
+    selfcareCurrentTime.innerText = `${data[5].timeframes.monthly.current}hrs`;
+    selfcarePreviousTime.innerText = `Last Week - ${data[5].timeframes.monthly.previous}hrs`
+});
